fd31b15128e3d7160552887ff2be15f437eefdb7
MSI\ichig <zeal24360155@fsgcl.jp>
2025-06-16 11:07:20 +0900

    10.4まで

--- src/shootinggame/GameFrame.java
+++ src/shootinggame/GameFrame.java
@@ -3,8 +3,13 @@ package shootinggame;
 public class GameFrame extends MyFrame {
 	public void run() {
 		GameWorld.player= new Player(100,300,0,0);
-		GameWorld.player.draw(this);
-
+		addKeyListener(GameWorld.player);//イベントリスナー登録(9章)
+		while(true) {
+			clear();
+			GameWorld.player.draw(this);
+			GameWorld.player.move();
+			sleep(0.03);
+		}
 	}
 	
 }
--- src/shootinggame/Player.java
+++ src/shootinggame/Player.java
@@ -1,11 +1,41 @@
 package shootinggame;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class Player extends Character {
+public class Player extends Character implements  KeyListener {
 
 	public Player(double x, double y, double vx, double vy) {
 		//Characterクラスのコンストラクタ呼び出し(8章)
 		super(x, y, vx, vy);
 		// TODO 自動生成されたコンストラクター・スタブ
 	}
+	public void keyPressed(KeyEvent e) {
+		//キーボードが押されたときの処理(9章)
+		if (e.getKeyCode()==KeyEvent.VK_LEFT) {
+			vx=-5;
+		
+		}
+		if (e.getKeyCode()==KeyEvent.VK_RIGHT) {
+			vx=5;
+		}
+	}
+	public void keyReleased(KeyEvent e) {
+		//キーボードが離されたときの処理(9章)
+		if(e.getKeyCode()==KeyEvent.VK_LEFT) {
+			vx=0;
+		}
+		if(e.getKeyCode()==KeyEvent.VK_RIGHT) {
+			vx=0;
+		}
+	}
+	public void keyTyped(KeyEvent e) {
+		
+	}
+	public void move() {
+		super.move();
+		if (x<0) x=0;
+		if (x>370) x=370;
+	}
+	
 
 }
